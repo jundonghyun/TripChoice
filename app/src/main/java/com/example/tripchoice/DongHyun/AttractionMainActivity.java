4ba0e321044e181ddbfb26f52d00ab731e743fc9
방일영 <tripchoice1111@gmail.com>
2021-11-24 19:04:41 +0900

    d

--- app/src/main/java/com/example/tripchoice/DongHyun/AttractionMainActivity.java
+++ app/src/main/java/com/example/tripchoice/DongHyun/AttractionMainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.tripchoice.R;
@@ -41,7 +43,6 @@ public class AttractionMainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("관광지추천");
         actionBar.setDisplayHomeAsUpEnabled(true);
-
 
 
         input_position = findViewById(R.id.input_position);
